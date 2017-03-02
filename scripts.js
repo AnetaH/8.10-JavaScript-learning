fd398d79b13f6b10401c84fe7a99f87e3286e393
Aneta Halz <aneta.halz@interia.eu>
2017-03-02 23:38:34 +0100

    add addEventListener

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
-window.onload = function() {
+document.addEventListener("DOMContentLoaded", function(event) {
   console.log('Strona w pełni załadowana.');
   var withButtonClass = document.getElementsByClassName('button');
 
   for (var a = 0; a < withButtonClass.length; a++) {
-    var text = withButtonClass[a].innerText;
-    console.log(text);
+    
+    console.log(withButtonClass[a].innerText);
   }
-}
+})
